475b43ab2bef79743f8e3bd827af6a22cf2e40e9
Miyazawa Akira <miyazawa-a@nii.ac.jp>
2017-03-08 06:00:07 +0000

    Fix test_syntax

--- trf/test/test_syntax.py
+++ trf/test/test_syntax.py
@@ -2,29 +2,37 @@
 
 import unittest
 
-from trf.tree import Syntax
+from trf.syntax import Syntax
 
 
-class TestTree(unittest.TestCase):
+class TestSyntax(unittest.TestCase):
 
-    def test_tree_depth_one(self):
+    def test_depth_one(self):
 
-        lines = ['ご飯を食べた',
-                 '顔を洗った']
-        tree = Syntax('\n'.join(lines), delimiter='\n')
-        self.assertAlmostEqual(tree.calc_mean_tree_depth(), 1.0)
+        lines = ['ご飯を食べた']
+        syntax = Syntax('\n'.join(lines), delimiter='\n')
+        self.assertAlmostEqual(int(syntax.calc_mean_tree_depth()), 1)
 
-    def test_tree_depth_two(self):
+    def test_depth_two(self):
 
         lines = ['踊る人を見た']
-        tree = Syntax('\n'.join(lines), delimiter='\n')
-        self.assertAlmostEqual(tree.calc_mean_tree_depth(), 2.0)
+        syntax = Syntax('\n'.join(lines), delimiter='\n')
+        self.assertEqual(int(syntax.calc_mean_tree_depth()), 2)
 
-    def test_tree_depth_tree(self):
+    def test_depth_three(self):
 
         lines = ['エサを食べるネコを眺めた']
-        tree = Syntax('\n'.join(lines), delimiter='\n')
-        self.assertAlmostEqual(tree.calc_mean_tree_depth(), 3.0)
+        syntax = Syntax('\n'.join(lines), delimiter='\n')
+        self.assertEqual(int(syntax.calc_mean_tree_depth()), 3)
+
+    def test_mean_depth(self):
+
+        lines = ['ご飯を食べた',
+                 '踊る人を見た',
+                 'エサを食べるネコを眺めた']
+        syntax = Syntax('\n'.join(lines), delimiter='\n')
+        self.assertAlmostEqual(syntax.calc_mean_tree_depth(), 2.0)
+
 
 if __name__ == '__main__':
     unittest.main()
