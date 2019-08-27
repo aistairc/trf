d5962cfab3f7754aa147256d6ddcac25dfacaca8
Miyazawa Akira <pecorarista@gmail.com>
2019-08-27 15:57:32 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
 from setuptools import setup, find_packages
 
-install_reqs = parse_requirements('requirements.txt', session='hack')
+requirements = parse_requirements('requirements.txt', session='hack')
 
 setup(name='trf',
       version='0.1',
@@ -12,5 +12,4 @@ setup(name='trf',
       author_email='{watanabe, murakami}@lr.pi.titech.ac.jp, miyazawa-a@nii.ac.jp',
       url='https://github.com/aistairc/trf',
       packages=find_packages("trf"),
-      install_requires=install_reqs)
-#      package_data={'', ['']},
+      install_requires=[str(requirement.req) for requirement in requirements])
